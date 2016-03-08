6ce24130f248092a74996655596216e34d0d2b7d
José Sequeira <sequeira@ua.pt>
2016-03-08 01:40:22 +0000

    Workshop tools added

---
+++ data.py
@@ -0,0 +1,26 @@
+#Generic Object Sequences (Lists and Dictionaries)
+
+lst = ["dog", "cat", "mouse", "goat", "albatross"]
+
+dic_lst = [{"animal": "dog", "street_cred": 5 }, {"animal": "cat", "street_cred": 9999 },
+{"animal": "mouse", "street_cred": 4 }, {"animal": "TMNT", "street_cred": 10000 }]
+
+add_lst = ["fish", "lepercoon", "shark"]
+
+
+#Mutable and Unmutable
+def func_str(str):
+	str += "add"
+	return str
+
+def func_lst(lst):
+	lst+= [1,2,3]
+	return lst
+
+def func_dic(dic):
+	dic["novo"] = ["INSERTED"]
+	return dic
+
+def func_int(n):
+	n+=2
+	return n
